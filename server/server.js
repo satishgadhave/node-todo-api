8e84ac0322e3018ab778a7b45c375f412758173e
satish <satish.gadhave@ambab.com>
2019-02-05 16:51:53 +0530

    post todo route

---
+++ server/server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var mongoose = require('./db/mongoose').mongoose;
+var Todo = require('./models/todo').Todo;
+var User = require('./models/user').User;
+
+var app = express();
+
+app.use(bodyParser.json());
+
+
+app.post('/todos', (req, res) => {
+    console.log(req.body.text);
+
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save()
+    .then((doc) => {
+        res.send(doc);
+    })
+    .catch( e => {
+        res.status(400).send(e);
+    });
+});
+
+app.listen(8002, () => {
+    console.log('Listening on port 8002...');
+});
+
